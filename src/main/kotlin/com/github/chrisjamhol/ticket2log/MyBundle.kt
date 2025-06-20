08cc6894424da3c25e903d31b5f308d8380979af
GitHub Action <action@github.com>
2025-06-20 18:58:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/chrisjamhol/ticket2log/MyBundle.kt
+++ src/main/kotlin/com/github/chrisjamhol/ticket2log/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.chrisjamhol.ticket2log
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/chrisjamhol/ticket2log/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/chrisjamhol/ticket2log/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.chrisjamhol.ticket2log.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
